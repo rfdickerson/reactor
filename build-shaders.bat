214f4d41fd192be2e0805e7165946e974ba1d4a0
Robert F. Dickerson <rfdickerson@gmail.com>
2025-08-08 16:21:58 -0500

    remove old shaders

--- build-shaders.bat
+++ build-shaders.bat
@@ -1,11 +1,5 @@
-glslc -g --target-env=vulkan1.3 -o resources/shaders/triangle.vert.spv shaders/triangle.vert
-glslc -g --target-env=vulkan1.3 -o resources/shaders/triangle.frag.spv shaders/triangle.frag
+slangc -g -O0 shaders/triangle.slang -target spirv -profile vs_6_0 -entry vertexMain -o resources/shaders/triangle-slang.vert.spv
+slangc -g -O0 shaders/triangle.slang -target spirv -profile ps_6_0 -entry fragmentMain -o resources/shaders/triangle-slang.frag.spv
 
-glslc -g --target-env=vulkan1.3 -o resources/shaders/composite.vert.spv shaders/composite.vert
-glslc -g --target-env=vulkan1.3 -o resources/shaders/composite.frag.spv shaders/composite.frag
-
-slangc -g shaders/triangle.slang -target spirv -profile vs_6_0 -entry vertexMain -o resources/shaders/triangle-slang.vert.spv
-slangc -g shaders/triangle.slang -target spirv -profile ps_6_0 -entry fragmentMain -o resources/shaders/triangle-slang.frag.spv
-
-slangc -g -O1 shaders/composite.slang -target spirv -profile vs_6_0 -entry vertexMain -o resources/shaders/composite-slang.vert.spv
-slangc -g -O1 shaders/composite.slang -target spirv -profile ps_6_0 -entry fragmentMain -o resources/shaders/composite-slang.frag.spv
+slangc -g -O0 shaders/composite.slang -target spirv -profile vs_6_0 -entry vertexMain -o resources/shaders/composite-slang.vert.spv
+slangc -g -O0 shaders/composite.slang -target spirv -profile ps_6_0 -entry fragmentMain -o resources/shaders/composite-slang.frag.spv
