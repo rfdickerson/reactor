63ac417a82fb6da3fc7e0b9f93c5d7b114788cc3
Robert F. Dickerson <rfdickerson@gmail.com>
2025-08-29 17:12:05 -0500

    allocator clean up

--- src/vulkan/Allocator.hpp
+++ src/vulkan/Allocator.hpp
@@ -2,54 +2,61 @@
 
 #include <vk_mem_alloc.h>
 #include <vulkan/vulkan.hpp>
-#include <functional>
 
-namespace reactor
-{
+#include <functional>
+#include <memory>
+
+namespace reactor {
 class Buffer;
 
-class Allocator
-{
+/// @brief Manages Vulkan memory allocations and associated resources.
+///   Non-copyable, responsible for buffer creation and allocation.
+class Allocator {
 public:
-    Allocator(vk::PhysicalDevice physicalDevice, vk::Device device, vk::Instance instance, vk::Queue graphicsQueue, uint32_t graphicsQueueFamilyIndex);
-    ~Allocator();
+  // @brief Constructs an Allocator for Vulkan resource management
+  Allocator(vk::PhysicalDevice physicalDevice,
+            vk::Device device,
+            vk::Instance instance,
+            vk::Queue graphicsQueue,
+            uint32_t graphicsQueueFamilyIndex);
 
-    VmaAllocator getAllocator() const
-    {
-        return m_allocator;
-    }
-    vk::Device getDevice() const
-    {
-        return m_device;
-    }
-    vk::Queue getGraphicsQueue() const
-    {
-        return m_graphicsQueue;
-    }
-    uint32_t getGraphicsQueueFamilyIndex() const
-    {
-        return m_graphicQueueFamilyIndex;
-    }
+  ~Allocator();
 
-    // New factory method
-    std::unique_ptr<Buffer> createBufferWithData(
-        const void* data,
-        vk::DeviceSize size,
-        vk::BufferUsageFlags usage,
-        const std::string& name = "");
+  Allocator(const Allocator&) = delete;
+  Allocator& operator=(const Allocator&) = delete;
+  Allocator(Allocator&&) = delete;
+  Allocator& operator=(Allocator&&) = delete;
 
-    // Non-copyable
-    Allocator(const Allocator&) = delete;
-    Allocator& operator=(const Allocator&) = delete;
+  [[nodiscard]] VmaAllocator getAllocator() const {
+    return m_allocator;
+  }
+
+  [[nodiscard]] vk::Device getDevice() const {
+    return m_device;
+  }
+
+  /// Return the graphics queue used for immediate submissions.
+  [[nodiscard]] vk::Queue getGraphicsQueue() const {
+    return m_graphicsQueue;
+  }
+
+  [[nodiscard]] uint32_t getGraphicsQueueFamilyIndex() const {
+    return m_graphicQueueFamilyIndex;
+  }
+
+  /// @brief Creates a buffer with given data and usage.
+  std::unique_ptr<Buffer> createBufferWithData(
+      const void* data,
+      vk::DeviceSize size,
+      vk::BufferUsageFlags usage,
+      const std::string& name = "");
 
 private:
+  void immediateSubmit(std::function<void(vk::CommandBuffer cmd)>&& function);
 
-    void immediateSubmit(std::function<void(vk::CommandBuffer cmd)>&& function);
-
-    VmaAllocator m_allocator = nullptr;
-    vk::Device m_device;
-    vk::Queue m_graphicsQueue;
-    uint32_t m_graphicQueueFamilyIndex;
+  VmaAllocator m_allocator = nullptr;
+  vk::Device m_device{};
+  vk::Queue m_graphicsQueue{};
+  uint32_t m_graphicQueueFamilyIndex{};
 };
-
 } // namespace reactor
